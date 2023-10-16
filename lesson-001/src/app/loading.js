7feeee8c5438c45082ca1a822fdcc5b68fe5c4d6
mukhriddin-dev <khodievm@gmail.com>
2023-10-16 10:03:45 +0500

    ok

---
+++ lesson-001/src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div>
+            <h1>LOADING  . . .</h1>
+        </div>
+    );
+};
+
+export default Loading;
---
+++ lesson-001/src/app/page.js
@@ -0,0 +1,9 @@
+const page = () => {
+    return (
+        <div className="p-4 border-2 border-green-600 m-4">
+            <h1>Home page</h1>
+        </div>
+    );
+};
+
+export default page;
